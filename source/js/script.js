43a77e7612b23e4175e428f9ae33acd051e58ffc
Streltsov Ivan <principdereva@gmail.com>
2023-02-18 23:17:02 +0100

    добавил джс

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,6 @@
 /* в этот файл добавляет скрипты*/
+const burgerButton = document.querySelector("#burger-button");
+function switchMenu() {
+  document.querySelector("nav").classList.toggle("header-nav--opened");
+}
+burgerButton.addEventListener("click", switchMenu);
